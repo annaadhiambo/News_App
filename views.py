f504ad902263513bdb29eddc6a932c612fb3c278
annaadhiambo <annaadhiambo01@gmail.com>
2020-09-12 17:34:26 +0300

    deleted main folder

---
+++ views.py
@@ -0,0 +1,36 @@
+from flask import render_template
+from newsapi import Newsapiclient
+from .import main
+
+# Views
+@main.route('/')
+def index():
+
+newsapi = Newsapiclient(api_key ="b6b97041f75646a49484f3adc0ff7156")
+everything = newsapi.get_top_everything(sources = "bbc-news")
+
+articles = everything["articles"]
+
+aut = []
+tit =[]
+descr =[]
+url =[]
+img =[]
+at =[]
+cont =[]
+
+for i in range(len(articles)):
+myarticles = articles[i]
+
+aut.append(myarticles['author'])
+tit.append(myarticles['title'])
+descr.append(myarticles['description'])
+url.append(myarticles['url'])
+img.append(myarticles['urlToImage'])
+at.append(myarticles['publishedAt'])
+cont.append(myarticles['content'])
+        
+
+    mytitle = zip(aut, tit, descr, url ,img ,at ,cont)
+
+    return render_template('index.html', context=mytitle)
