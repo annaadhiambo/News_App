d03595f2e2038a582873912d318bf3b300a6832f
annaadhiambo <annaadhiambo01@gmail.com>
2020-09-13 10:24:29 +0300

    updated my README file

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 ## Description
 * An application that will help someone who never had the chance to be able to watch news on time.This is a project that enable users to get access to various news sources and articles in the world
 
+## Features
+* Landing page containing news sources
+* Clickable news headlines
+* Articles page to see varous articles in a source
+* Navbar
+
 ## Technology Used
 * Python
 * Pip
@@ -16,6 +22,19 @@
 ## Support and contact details
 * contact@ annaadhiambo01@gmail.com
 
-## Featuers 
-* Navbar
+## How to access it 
+You can access it by cloning the [Repository](https://github.com/annaadhiambo/News_App.git)
+```
+$ Open Terminal (Ctrl Alt T)
+$ git clone link 
+$ cd directory-name(Password)
+$ code .
+```
+
+
+## How to contribute
+You can fork the project from the git hub and clone it by following the above instruction
+## License and Copyright
+Licensed under[MIT License](LICENSE)Copyright &2020
+
 
--- app/views.py
+++ app/views.py
@@ -1,36 +1,44 @@
-from flask import render_template
-from newsapi import Newsapiclient
+from flask import Flask,render_template
+from newsapi import NewsApiClient
+import os
+from app import app
 
+
+app = Flask(__name__)
 
 # Views
 @app.route('/')
 def index():
 
-newsapi = Newsapiclient(api_key ="b6b97041f75646a49484f3adc0ff7156")
-everything = newsapi.get_top_everything(sources = "bbc-news")
+    newsapi = NewsApiClient(api_key ="b6b97041f75646a49484f3adc0ff7156")
+    top_headlines = newsapi.get_top_headlines(q='trump')
 
-articles = everything["articles"]
+    articles = top_headlines["articles"]
 
-aut = []
-tit =[]
-descr =[]
-url =[]
-img =[]
-at =[]
-cont =[]
+    aut = []
+    tit =[]
+    descr =[]
+    url =[]
+    img =[]
+    at =[]
+    cont =[]
 
-for i in range(len(articles)):
-myarticles = articles[i]
+    for i in range(len(articles)):
+        myarticles = articles[i]
 
-aut.append(myarticles['author'])
-tit.append(myarticles['title'])
-descr.append(myarticles['description'])
-url.append(myarticles['url'])
-img.append(myarticles['urlToImage'])
-at.append(myarticles['publishedAt'])
-cont.append(myarticles['content'])
-        
+        aut.append(myarticles['author'])
+        tit.append(myarticles['title'])
+        descr.append(myarticles['description'])
+        url.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+        at.append(myarticles['publishedAt'])
+        cont.append(myarticles['content'])
+                
 
-    mytitle = zip(aut, tit, descr, url ,img ,at ,cont)
+        mylist = zip(aut, tit, descr, url ,img ,at ,cont)
 
-    return render_template('index.html', context=mytitle)
+        return render_template('index.html', context=mylist)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run
