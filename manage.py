4ffe258352b0a4125adb7c230f76e84b18672009
annaadhiambo <annaadhiambo01@gmail.com>
2020-09-14 16:48:40 +0300

    uninstalled vitual and installed new one

--- manage.py
+++ manage.py
@@ -1,16 +1,12 @@
 from flask import Flask,render_template
 from app.requests import NewsRequests
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     news = NewsRequests()
     news= news.get_top_headlines( sources='')
     # return news
     return render_template('index.html',articles=news['articles'])
-    return '<h1 style="color:red; text-align:center; font-size:70px; background-color:grey; height:20vh;" > World News </h1>'
-
-    
-
-    
 if __name__ == '__main__':
     app.run(debug = True)
---
+++ start.sh
@@ -0,0 +1,1 @@
+export NEWS_API_KEY='b6b97041f75646a49484f3adc0ff7156'
