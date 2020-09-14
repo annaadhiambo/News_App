068ee23c8856baf69ca5e41edeef2f3812e92385
annaadhiambo <annaadhiambo01@gmail.com>
2020-09-14 12:44:13 +0300

    modified files

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     news = NewsRequests()
-    news= news.get_top_headlines( sources='bbc-news')
+    news= news.get_top_headlines( sources='')
     # return news
     return render_template('index.html',articles=news['articles'])
     return '<h1 style="color:red; text-align:center; font-size:70px; background-color:grey; height:20vh;" > World News </h1>'
